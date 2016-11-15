744813845d3b41bc5d8872dbe5028fa0a5370cb5
richardastantonjr <rich_stanton2003@yahoo.com>
2016-11-15 15:53:38 -0500

    assigned data to an object
    loaded stringr library and used str_tolower function to make the function that follows case insensitive- grabs the dnaseq and converts to lowercase
    
    Loaded the get_gc_content function from an earlier exercise to process dnaseq
    
    tested the function

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,13 @@
 ## do something with house elf data
 
-read.csv("./data/houseelf_earlength_dna_data_1.csv.csv", stringsAsFactors = FALSE)
+elf_data_1<-read.csv("./data/houseelf_earlength_dna_data_1.csv.csv", stringsAsFactors = FALSE)
+
+library(stringr)
+
+dnaseq<-str_to_lower(elf_data_1$dnaseq)
+get_gc_content<- function (sequences){
+  gc_content<-(str_count(dnaseq,"g") +  str_count(dnaseq, "c"))/str_length(sequences) * 100
+  return(gc_content)
+}
+
+get_gc_content(dnaseq)
