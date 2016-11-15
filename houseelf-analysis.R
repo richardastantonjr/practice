c0592d64659c4468a2f490014133fd0dbb73fe06
richardastantonjr <rich_stanton2003@yahoo.com>
2016-11-15 15:45:10 -0500

    wrote csv twice. duh.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,3 @@
 ## do something with house elf data
 
-read.csv("./data/houseelf_earlength_dna_data_1.csv", stringsAsFactors = FALSE)
+read.csv("./data/houseelf_earlength_dna_data_1.csv.csv", stringsAsFactors = FALSE)
